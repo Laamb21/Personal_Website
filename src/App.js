05a9d3c2450d44a7fa73f662ae6acab634d88574
Liam Slade <wslade1@students.towson.edu>
2024-06-26 13:22:38 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Router } from "express";
 import React from "react";
 
 function App() {
-  return null
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path ="/" exact component={Home} />
+          <Route path ="/about" component={About} />
+          <Route path ="/projects" component={Projects} />
+        </Switch>
+        <Footer/>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; Liam Slade</p>
+    </footer>
+  );
+}
+
+export default Footer;
