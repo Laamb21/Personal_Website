1a1ac0628ea7386e1bec4108895e0f9d665e9576
Liam Slade <wslade1@students.towson.edu>
2024-06-27 19:37:49 -0400

    fixed some routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { Router } from "express";
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Switch>
+        <Routes>
           <Route path ="/" exact component={Home} />
           <Route path ="/about" component={About} />
           <Route path ="/projects" component={Projects} />
-        </Switch>
+        </Routes>
         <Footer/>
       </div>
     </Router>
