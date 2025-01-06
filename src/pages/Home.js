6d29ae5f10648a4b2fa083fffea8a6d4274990a7
Liam Slade <wslade1@students.towson.edu>
2025-01-06 14:37:34 -0500

    added headshot to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Home.css'; // Make sure to create this CSS file
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <h1>Hey, I'm Liam</h1>
+      <img src="/images/my-headshot.png" alt="My Headshot" className="center-image"/>
       <p>This is the home page.</p>
     </div>
   );
