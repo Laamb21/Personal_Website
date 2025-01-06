d5652e2af2c6a2ac2955dc6fcc7a0bd0463f3212
Liam Slade <wslade1@students.towson.edu>
2025-01-06 10:43:54 -0500

    restructured project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from "./components/Footer";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Home from "./components/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Home from "./pages/Home";
 
 function App() {
   return (
