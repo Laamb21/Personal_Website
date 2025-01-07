eb4b704c81e07a457dfe6e771cc8b19e7162a1e0
Liam Slade <wslade1@students.towson.edu>
2025-01-07 18:25:23 -0500

    added buttons to the left of headshot, still adjusting orientation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,16 @@ import './Home.css'; // Make sure to create this CSS file
 function Home() {
   return (
     <div className="home-container">
-      <h1>Hey, I'm Liam</h1>
-      <img src="/images/my-headshot.png" alt="My Headshot" className="center-image"/>
-      <p>This is the home page.</p>
+      <div className="sidebar">
+        <button className="nav-button" onClick={() => window.location.href = '/about'}>About</button>
+        <button className="nav-button" onClick={() => window.location.href = '/projects'}>Projects</button>
+        <button className="nav-button" onClick={() => window.location.href = '/contact'}>Contact</button>
+      </div>
+      <div className="main-content">
+        <h1 className="home-title">Hey, I'm Liam</h1>
+        <img src="/images/my-headshot.png" alt="My Headshot" className="center-image"/>
+        <p className="home-text">This is the home page.</p>
+      </div>
     </div>
   );
 }
